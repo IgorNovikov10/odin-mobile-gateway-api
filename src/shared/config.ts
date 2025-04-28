0f3cd851a40a806f409443ba0a6b5aa0db789cf9
IgorNovikov10 <igor.novikov@ukad-group.com>
2025-04-28 14:57:36 +0300

    config and scope updates for auth

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   signicat: {
     baseUrl: "https://odinfondene.sandbox.signicat.com/auth/open/connect",
-    clientId: "dev.odinfondene.mobileid",
+    clientId: "sandbox-precious-goat-757",
     redirectUri:
       "https://odin-mobile-gateway-api.onrender.com/api/v1/oidc/verify",
-    scope: "openid profile mobileid signicat.national_id",
+    scope: "openid mobileid nin profile",
     responseType: "code",
     prompt: "login",
-    acrValue: "urn:signicat:oidc:method:nbid",
+    acrValue: "idp:nbid",
     locale: "no",
   },
 };
