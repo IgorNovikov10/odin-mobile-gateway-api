93808c601b1327a3467b90ba82e5c7b77aee6b8d
IgorNovikov10 <igor.novikov@ukad-group.com>
2025-04-28 14:12:56 +0300

    updated config, gitignore added

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   signicat: {
-    baseUrl: "https://preprod.signicat.com/oidc",
+    baseUrl: "https://odinfondene.sandbox.signicat.com/auth/open/connect",
     clientId: "preprod.odinfondene.mobileid",
     redirectUri: "http://localhost:3000/api/v1/oidc/verify",
     scope: "openid profile mobileid signicat.national_id",
