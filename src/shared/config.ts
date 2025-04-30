22ba20e26c73a2096c5d6ff3321516f8957acb11
IgorNovikov10 <igor.novikov@ukad-group.com>
2025-04-30 11:42:17 +0300

    additional scope for oidc client

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -6,7 +6,7 @@ export const config = {
     clientSecret: "ee5XhnpU3Obi85fWa3LINjy7m3picZqIuldjSv2dfSiTbu3f",
     redirectUri:
       "https://odin-mobile-gateway-api.onrender.com/api/v1/oidc/verify",
-    scope: "openid mobileid nin profile",
+    scope: "openid mobileid nin profile nbid-extra",
     responseType: "code",
     prompt: "login",
     acrValue: "idp:nbid",
